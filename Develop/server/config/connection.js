fe0dc82ab97453311e1ef26d3538ebe8390d7ba2
thejackbitt <jack.bittner@outlook.com>
2024-03-06 20:37:46 -0600

    production changes

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://thejackbitt:Dubbystep1@cluster0.4woxjjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
